9162e2d8d31f359741a8a4374ced18ade8c26e20
hirs <hirsalexander@gmail.com>
2017-04-24 08:17:54 +0300

    hello

--- src/main/java/com/hirs/AppInit.java
+++ src/main/java/com/hirs/AppInit.java
@@ -2,11 +2,12 @@ package com.hirs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value = "com.hirs")
-public class AppInit {
+public class AppInit extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AppInit.class, args);
--- src/main/java/com/hirs/controller/HelloController.java
+++ src/main/java/com/hirs/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.hirs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
     }
-
 }
