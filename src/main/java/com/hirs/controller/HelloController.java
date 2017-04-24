174b0e18174909faa42e098d538f7e71f37f14e4
hirs <hirsalexander@gmail.com>
2017-04-24 19:27:20 +0300

    thymeleaf

--- src/main/java/com/hirs/controller/HelloController.java
+++ src/main/java/com/hirs/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.hirs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/")
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
+        LOGGER.info("/hello");
         return "hello";
     }
 }
