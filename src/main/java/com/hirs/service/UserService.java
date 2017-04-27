fc02179670e34dcc4bb5fd3a1550ed6d02ceb5c5
hirs <hirsalexander@gmail.com>
2017-04-27 08:23:32 +0300

    database connect11

--- src/main/java/com/hirs/service/UserService.java
+++ src/main/java/com/hirs/service/UserService.java
@@ -1,12 +1,12 @@
-package com.hirs.service;
-
-import com.hirs.model.app.User;
-import com.hirs.model.app.VendorId;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import java.util.List;
-
-public interface UserService extends UserDetailsService {
-    User getUserByLogin(String login);
-    List<VendorId> getRegisteredVendorIds();
-}
+//package com.hirs.service;
+//
+//import com.hirs.model.app.User;
+//import com.hirs.model.app.VendorId;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//
+//import java.util.List;
+//
+//public interface UserService extends UserDetailsService {
+//    User getUserByLogin(String login);
+//    List<VendorId> getRegisteredVendorIds();
+//}
